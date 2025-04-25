491ad3147538ada3e6dad69c33d938f608e40427
Josh Gillette <joshtgillette@gmail.com>
2025-04-25 10:29:54 -0500

    Disable tagline request until backend deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 export default async function Home() {
-  const res = await fetch(`${process.env.BACKEND_ORIGIN}/tagline`);
-  const tagLine = await res.text();
+  // const res = await fetch(`${process.env.BACKEND_ORIGIN}/tagline`);
+  // const tagLine = await res.text();
 
   return (
     <div className="h-screen flex flex-col items-center justify-center">
       <div className="mt-[-150px] text-[200px]">
         jenet.ai
       </div>
-      <div className="text-[25px]" dangerouslySetInnerHTML={{ __html: tagLine }}/>
+      {/* <div className="text-[25px]" dangerouslySetInnerHTML={{ __html: tagLine }}/> */}
     </div>
   )
 }
