f901424a770f6bf0ecb2a4b73035a85feadb5958
Josh Gillette <joshtgillette@gmail.com>
2025-04-26 16:37:39 -0500

    Minor home page style tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ export default async function Home() {
 
   return (
     <div className="h-screen flex flex-col items-center justify-center">
-      <div className="mt-[-150px] text-[200px]">
+      <div className="mt-[-150px] text-[25vmin]">
         jenet.ai
       </div>
-      <div className="text-[25px]" dangerouslySetInnerHTML={{ __html: tagLine }}/>
+      <div className="text-[2.5vh] text-center" dangerouslySetInnerHTML={{ __html: tagLine }}/>
     </div>
   )
 }
