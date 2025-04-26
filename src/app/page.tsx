ad9c99af96ede56f456317d210eb1969818304dd
Josh Gillette <joshtgillette@gmail.com>
2025-04-26 16:25:06 -0500

    Squashed commit of the following:
    commit 4efcad284dc731823ecbe7e371f71653758a1743
    Author: Josh Gillette <joshtgillette@gmail.com>
    Date:   Sat Apr 26 16:11:47 2025 -0500
    
        Refactor backend API url env variable name
    
    commit 6a2175a764dc853913e976a948b0a05744859ae0
    Author: Josh Gillette <joshtgillette@gmail.com>
    Date:   Fri Apr 25 11:02:36 2025 -0500
    
        Change home page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 export default async function Home() {
-  // const res = await fetch(`${process.env.BACKEND_ORIGIN}/tagline`);
-  // const tagLine = await res.text();
+  const res = await fetch(`${process.env.BACKEND_API_URL}/tagline`);
+  const tagLine = await res.text();
 
   return (
     <div className="h-screen flex flex-col items-center justify-center">
       <div className="mt-[-150px] text-[25vmin]">
         jenet ai
       </div>
-      <div className="text-[5vmin]"><i>coming soon</i></div>
+      <div className="text-[25px]" dangerouslySetInnerHTML={{ __html: tagLine }}/>
     </div>
   )
 }
