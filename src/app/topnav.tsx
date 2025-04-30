a19f4df508790fbe34f72d926f0c4eeb043e44bd
Josh Gillette <joshtgillette@gmail.com>
2025-04-30 11:25:17 -0500

    Tweak topnav styling

--- src/app/topnav.tsx
+++ src/app/topnav.tsx
@@ -6,9 +6,9 @@ const TopNav = () => {
     <div className="fixed z-50 w-full flex items-center justify-center gap-[50px] p-[15px] text-lg">
         <div className="
             flex flex-grow justify-between
-            max-w-[600px] px-[20px] py-[10px] dark:text-white
-            bg-[rgb(245,245,245)] dark:bg-[rgb(30,30,30)] hover:bg-[rgb(248,248,248)] dark:hover:bg-[rgb(33,33,33)]
-            border-1 border-black dark:border-white
+            max-w-[600px] px-[20px] py-[15px] dark:text-white
+            hover:bg-[rgb(250,250,250)] dark:hover:bg-[rgb(20,20,20)]
+            border-2 border-black dark:border-white
             rounded-xl"
         >
             <a>why?</a>
