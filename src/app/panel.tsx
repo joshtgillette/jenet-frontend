b74bee500311f91ac791ca11d604384989dcd82c
Josh Gillette <joshtgillette@gmail.com>
2025-06-11 16:50:44 -0500

    Fully adopt glass-morphism design language

--- src/app/panel.tsx
+++ src/app/panel.tsx
@@ -15,7 +15,7 @@ const Panel = ({ content, className }: PanelProps) => {
     return (
         <div
             className={
-                `group max-w-200 min-w-110 relative flex flex-col p-4 rounded-xl bg-[rgb(255,255,255)]/85 shadow-[0_0_2px_1px_rgba(120,120,180,0.2)] transition-shadow duration-300 overflow-auto hover:bg-[rgb(255,255,255)] hover:shadow-[0_0_10px_1px_rgba(120,120,180,0.2)] ${className ?? ""}`
+                `glass group max-w-200 min-w-110 relative flex flex-col p-4 rounded-xl overflow-auto ${className ?? ""}`
             }
         >
             {content}
